a8ba89dcc971e5e414b6a9ac764d9696f932d70f
Tiago <tiago@Tiagos-MacBook-Pro.local>
2016-03-29 10:52:47 -0300

    First class, requirejs config

---
+++ src/js/config/requireConfig.js
@@ -0,0 +1,24 @@
+require.config({
+    baseUrl: "js",
+    paths: {
+        'jquery': '../../bower_components/jquery/dist/jquery.min',
+        'underscore': '../../bower_components/underscore/underscore-min',
+        'backbone': '../..//bower_components/backbone/backbone-min',
+        'marionette': '../../bower_components/backbone.marionette/lib/backbone.marionette.min'
+    },
+
+    shim: {
+        backbone: {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        marionette: {
+            exports: 'Backbone.Marionette',
+            deps: ['backbone']
+        },
+        underscore: {
+            'exports': '_'
+        }
+    }
+
+});
