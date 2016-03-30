42e1c9de90e7e7776e490515071a492f6686bbe3
Tiago <tiago@Tiagos-MacBook-Pro.local>
2016-03-30 10:34:06 -0300

    Second class, templates and form view start

---
+++ src/js/views/loginFormView.js
@@ -0,0 +1,3 @@
+define(['backbone', 'marionette', 'models/loginModel', 'text!templates/loginFormTemplate'], function(Backbone, Marionette, LoginModel, LoginFormTemplate){
+  return Marionette.ItemView.extend({});
+});
