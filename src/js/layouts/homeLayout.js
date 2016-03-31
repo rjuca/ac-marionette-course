7224fc30e6e94d1cf476ef27cd5f65d06d05f8a2
Tiago <tiago@Tiagos-MacBook-Pro.local>
2016-03-31 11:08:52 -0300

    Third Class, Profile View

--- src/js/layouts/homeLayout.js
+++ src/js/layouts/homeLayout.js
@@ -1,12 +1,25 @@
-define(['marionette', 'backbone'], function(Marionette, Backbone){
+define(['marionette', 'backbone', 'text!templates/homeTemplate', 'models/userModel', 'views/userView'], function(Marionette, Backbone, HomeTemplate, UserModel, UserView){
   return Marionette.LayoutView.extend({
-    template: '<div> Home Screen </div>',
+    template: HomeTemplate,
     regions: {
       profile: '#profile',
       userList: '#userList'
     },
     initialize: function(){
-
+      this.userModel = new UserModel({
+        id: 'me'
+      });
+      this.listenTo(this.userModel, 'sync', this.showProfileView);
+      this.userModel.fetch({
+        headers: {
+          'Authorization' : 'bearer ' + sessionStorage.sessionId
+        }
+      });
+    },
+    showProfileView: function(){
+      this.showChildView('profile', new UserView({
+        model: this.userModel
+      }));
     }
   });
 });
